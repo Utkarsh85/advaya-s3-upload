6074bc8569eda22bc3a9b0cada67df6226128abd
Utkarsh85 <utkarshsharma8519@gmail.com>
2016-09-18 05:03:08 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+var fileS3 = require('./lib/fileS3');
+var imageS3 = require('./lib/imageS3');
+var imageCropS3 = require('./lib/imageCropS3');
+
+module.exports={
+	file: fileS3,
+	image: imageS3,
+	imageCrop: imageCropS3
+}
